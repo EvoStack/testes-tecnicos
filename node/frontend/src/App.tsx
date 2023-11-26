4610078429077b9275378392c4bee3ddea4d3502
jaimeflneto <jaimeflneto@gmail.com>
2023-11-25 21:24:58 -0300

    estrutura de rotas (react-router-dom) + tailwindcss

--- node/frontend/src/App.tsx
+++ node/frontend/src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+
 import "./App.css";
+import Routes from "./routes";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
-}
+const App = () => <Routes />;
 
 export default App;
---
+++ node/frontend/src/routes.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard";
+import { StocksDetails } from "./pages/stockDetails";
+import { QueryClientProvider, QueryClient } from "react-query";
+const queryClient = new QueryClient();
+const routes = () => (
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route index element={<Dashboard />} />
+        <Route path={"/stocks/:stockName"} element={<StocksDetails />} />
+        <Route path="/about" element={<Dashboard />} />
+      </Routes>
+    </QueryClientProvider>
+  </BrowserRouter>
+);
+
+export default routes;
