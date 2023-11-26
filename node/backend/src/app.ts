ffb59493ab0c22e83795d2f1d1f7ed3d6237d7ac
jaimeflneto <jaimeflneto@gmail.com>
2023-11-25 21:23:59 -0300

    hotfix backend cors

--- node/backend/src/app.ts
+++ node/backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
+var cors = require('cors');
 import routes from './routes';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use(
